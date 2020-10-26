894cc94c6137df2eec56f1431673819381c74676
UlrichKreher <Ulrich.Kreher@aristaflow.com>
2020-10-26 09:29:05 +0100

    Add JUnit-test for parsing German character country code.

--- src/test/java/com/innovatrics/mrz/MrzFinderUtilTest.java
+++ src/test/java/com/innovatrics/mrz/MrzFinderUtilTest.java
@@ -9,9 +9,12 @@ import org.junit.Test;
 public class MrzFinderUtilTest {
 
 	private static final String VALID_MRZ = "I<SVKNOVAK<<JAN<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n123456<AA5SVK8110251M1801020749313<<<<<<<<70";
+	private static final String VALID_GER_MRZ = "P<D<<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<<<<<<<<<\nC01X00T478D<<6408125F2702283<<<<<<<<<<<<<<<4";
 	private static final String INVALID_MRZ = "I<SVKNOVAK<<JAN<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n123456";
 	private static final String WRAPPED_VALID = "xx\n\nyyy\n" + VALID_MRZ + "\nZZZZ";
+  private static final String WRAPPED_VALID_GER_MRZ = "xx\n\nyyy\n" + VALID_GER_MRZ + "\nZZZZ";
 	private static final String WRAPPED_INVALID_MRZ = "XX\nAZ09<\nYYY\n" + INVALID_MRZ + "\nAZ09<\nZZZZ";
+	private static final String NO_GER_MRZ = "P<DE<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<<<<<<<<<\nC01X00T478D<<6408125F2702283<<<<<<<<<<<<<<<4";
 	private static final String NO_MRZ = "AZ09<\n\nBBB\n\nAZ09<\nCCCCC";
 
 	@Test
@@ -20,8 +23,18 @@ public class MrzFinderUtilTest {
 	}
 
 	@Test
+	public void testValidGerMrz() throws MrzNotFoundException, MrzParseException {
+	  Assert.assertEquals("Did not find valid MRZ", VALID_GER_MRZ, MrzFinderUtil.findMrz(VALID_GER_MRZ));
+	}
+
+	@Test
 	public void testValidWrappedMrz() throws MrzNotFoundException, MrzParseException {
 		Assert.assertEquals("Did not find valid wrapped MRZ", VALID_MRZ, MrzFinderUtil.findMrz(WRAPPED_VALID));
+	}
+
+	@Test
+	public void testValidWrappedGerMrz() throws MrzNotFoundException, MrzParseException {
+	  Assert.assertEquals("Did not find valid wrapped MRZ", VALID_GER_MRZ, MrzFinderUtil.findMrz(WRAPPED_VALID_GER_MRZ));
 	}
 
 	@Test(expected = MrzParseException.class)
@@ -38,6 +51,11 @@ public class MrzFinderUtilTest {
 	public void testNotFoundMrz() throws MrzNotFoundException, MrzParseException {
 		MrzFinderUtil.findMrz(NO_MRZ);
 	}
+
+  @Test(expected = MrzNotFoundException.class)
+  public void testNotFoundGerMrz() throws MrzNotFoundException, MrzParseException {
+    MrzFinderUtil.findMrz(NO_GER_MRZ);
+  }
 
 	@Test(expected = MrzNotFoundException.class)
 	public void testNullMrz() throws MrzNotFoundException, MrzParseException {
